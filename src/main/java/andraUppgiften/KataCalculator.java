60525dd233f5a76ddd1ad38b43f7298ba81b079d
bobster <bob.hogvall@iths.se>
2023-02-07 12:18:33 +0100

    create code to first test

--- src/main/java/andraUppgiften/KataCalculator.java
+++ src/main/java/andraUppgiften/KataCalculator.java
@@ -1,22 +1,11 @@
 package andraUppgiften;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
     public class KataCalculator {
 
 
         public static int Add(String numbers){
-//            var sum = Stream(Stream.of(numbers.split(","))
-//                            .
-//
-//            //null pointer exception
-
-            return 0;
+            return Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).sum();
         }
-
 
     }
